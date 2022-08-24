98f3ad5a90ab14a6ba65ccb01d00854073e4294e
malemena <mcmena@fi.uba.ar>
2022-08-24 13:59:54 -0300

    Creación carpeta Back End y primer test

---
+++ ProyectoFS/Back End/src/main/java/com/proyecto/malena/MalenaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.malena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalenaApplication.class, args);
+	}
+
+}
